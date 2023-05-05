f9373a03856b95ad266ad5ef4b00da3bdb6af174
tedet <tedteah@gmail.com>
2023-05-05 17:12:39 -0400

    finished artist page

--- src/main.js
+++ src/main.js
@@ -5,6 +5,14 @@ import store from './store'
 
 import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
 
+/* import the fontawesome core */
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+import { far } from '@fortawesome/free-regular-svg-icons'
+
+library.add(fas, fab, far)
 Vue.use(BootstrapVue)
 Vue.use(BootstrapVueIcons)
 
--- src/views/Artists.vue
+++ src/views/Artists.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="container">
-    <h4>Horizontal Flip</h4>
+    <!-- <h4>Horizontal Flip</h4> -->
     <div v-for="(artist, index) in artists" :key="index" class="flip">
       <div class="front">
-        <h3 class="text-shadow">{{ artist.name }}</h3>
-        <img style="height: 150px" :src="artist.photo1" />
+        <h3 id="example5" class="text-shadow">{{ artist.name }}</h3>
+        <img style="height: 125px" :src="artist.photo1" />
       </div>
       <div class="back">
         <!-- Title -->
@@ -17,39 +17,34 @@
         <p v-if="artist.dba">{{ artist.dba }}</p>
 
         <!-- About me -->
-        <p class="artisttext">{{ artist.artistStatement }}</p>
+        <p  class="artisttext">{{ artist.artistStatement }}</p>
 
         <!-- Website -->
-        <a v-if="artist.website" :href="artist.website" alt="homepage" style="color: black; padding-right: 10px">
-          <b-icon-house></b-icon-house
-        ></a>
+        <a v-if="artist.website" :href="artist.website" alt="homepage" style="color: black; font-size: 24px;padding-right: 5px">
+          <b-icon-house></b-icon-house></a>
 
         <!-- Twitter Link -->
-        <a v-if="artist.twitter" :href="artist.twitter" alt="twitter" style="color: black; padding-right: 10px"
-          ><font-awesome-icon :icon="{ prefix: 'fab', iconName: 'twitter' }"
-        /></a>
+        <a v-if="artist.twitter" :href="artist.twitter" alt="twitter" style="font-size: 24px;color: black; padding-right: 5px"
+          ><b-icon-twitter></b-icon-twitter></a>
 
         <!-- Facebook Link -->
-        <a v-if="artist.facebook" class="link-light" :href="artist.facebook" alt="FB" style="color: black; padding-right: 10px"
-          ><font-awesome-icon :icon="{ prefix: 'fab', iconName: 'facebook' }"
-        /></a>
+        <a v-if="artist.facebook" class="link-light" :href="artist.facebook" alt="FB" style="font-size: 24px;   color: black; padding-right: 5px""
+          ><b-icon-facebook></b-icon-facebook></a>
 
-        <!-- Squarestore Link 
-                        <a class="link-light" :href="artist.SquareStore" alt="squareB" style="color:black; padding-right: 10px;"><font-awesome-icon :icon="{ prefix: 'fa', iconName: 'square'}"/></a>
-                    -->
-
+  
         <!-- Instragram Link -->
-        <a v-if="artist.Instagram" :href="artist.Instagram" alt="insta" style="color: black; padding-right: 10px"
-          ><font-awesome-icon :icon="{ prefix: 'fab', iconName: 'instagram' }"
-        /></a>
+        <a v-if="artist.Instagram" :href="artist.Instagram" alt="insta" style="font-size: 24px;color: black; padding-right: 5px"
+          ><b-icon-instagram></b-icon-instagram></a>
 
         <!-- Esty Store -->
-        <a v-if="artist.EtsayStore" :href="artist.EtsayStore" alt="etsy" style="color: black; padding-right: 10px"
-          ><font-awesome-icon :icon="{ prefix: 'fab', iconName: 'etsy' }"
-        /></a>
+        <a v-if="artist.EtsayStore" :href="artist.EtsayStore" alt="etsy" style="color: black; padding-right: 5px"
+          ><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-explicit" viewBox="0 0 16 16">
+  <path d="M6.826 10.88H10.5V12h-5V4.002h5v1.12H6.826V7.4h3.457v1.073H6.826v2.408Z"/>
+  <path d="M2.5 0A2.5 2.5 0 0 0 0 2.5v11A2.5 2.5 0 0 0 2.5 16h11a2.5 2.5 0 0 0 2.5-2.5v-11A2.5 2.5 0 0 0 13.5 0h-11ZM1 2.5A1.5 1.5 0 0 1 2.5 1h11A1.5 1.5 0 0 1 15 2.5v11a1.5 1.5 0 0 1-1.5 1.5h-11A1.5 1.5 0 0 1 1 13.5v-11Z"/>
+</svg></a>
 
         <!-- Email -->
-        <a :href="'mailto:' + artist.email"> <i style="color: black" class="fa fa-paper-plane" aria-hidden="true"></i> </a>
+        <a :href="'mailto:' + artist.email">  <b-icon-envelope style="font-size: 30px; color: black;"></b-icon-envelope></a>
       </div>
     </div>
 
