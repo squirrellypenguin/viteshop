ea89de3837505a74510e496852efa9ce7943a7e3
tedet <tedteah@gmail.com>
2023-05-05 17:17:26 -0400

    finished artist page

--- src/main.js
+++ src/main.js
@@ -6,8 +6,8 @@ import store from './store'
 import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
 
 /* import the fontawesome core */
-import { fas } from '@fortawesome/free-solid-svg-icons'
 import { fab } from '@fortawesome/free-brands-svg-icons'
+import { fas } from '@fortawesome/free-solid-svg-icons'
 import { library } from '@fortawesome/fontawesome-svg-core'
 
 import { far } from '@fortawesome/free-regular-svg-icons'
--- src/views/Artists.vue
+++ src/views/Artists.vue
@@ -17,32 +17,60 @@
         <p v-if="artist.dba">{{ artist.dba }}</p>
 
         <!-- About me -->
-        <p  class="artisttext">{{ artist.artistStatement }}</p>
+        <p class="artisttext">{{ artist.artistStatement }}</p>
 
         <!-- Website -->
-        <a v-if="artist.website" :href="artist.website" alt="homepage" style="color: black; font-size: 24px;padding-right: 5px">
-          <b-icon-house></b-icon-house></a>
+        <a
+          v-if="artist.website"
+          :href="artist.website"
+          alt="homepage"
+          style="color: black; font-size: 24px; padding-right: 5px"
+        >
+          <b-icon-house></b-icon-house
+        ></a>
 
         <!-- Twitter Link -->
-        <a v-if="artist.twitter" :href="artist.twitter" alt="twitter" style="font-size: 24px;color: black; padding-right: 5px"><b-icon-twitter></b-icon-twitter></a>
+        <a v-if="artist.twitter" :href="artist.twitter" alt="twitter" style="font-size: 24px; color: black; padding-right: 5px"
+          ><b-icon-twitter></b-icon-twitter
+        ></a>
 
         <!-- Facebook Link -->
-        <a v-if="artist.facebook" class="link-light" :href="artist.facebook" alt="FB" style="font-size: 24px; color: black; padding-right: 5px"><b-icon-facebook></b-icon-facebook>
+        <a
+          v-if="artist.facebook"
+          class="link-light"
+          :href="artist.facebook"
+          alt="FB"
+          style="font-size: 24px; color: black; padding-right: 5px"
+          ><b-icon-facebook></b-icon-facebook
+        ></a>
 
-  
         <!-- Instragram Link -->
-        <a v-if="artist.Instagram" :href="artist.Instagram" alt="insta" style="font-size: 24px;color: black; padding-right: 5px"
-          ><b-icon-instagram></b-icon-instagram></a>
+        <a
+          v-if="artist.Instagram"
+          :href="artist.Instagram"
+          alt="insta"
+          style="font-size: 24px; color: black; padding-right: 5px"
+          ><b-icon-instagram></b-icon-instagram
+        ></a>
 
         <!-- Esty Store -->
         <a v-if="artist.EtsayStore" :href="artist.EtsayStore" alt="etsy" style="color: black; padding-right: 5px"
-          ><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-explicit" viewBox="0 0 16 16">
-  <path d="M6.826 10.88H10.5V12h-5V4.002h5v1.12H6.826V7.4h3.457v1.073H6.826v2.408Z"/>
-  <path d="M2.5 0A2.5 2.5 0 0 0 0 2.5v11A2.5 2.5 0 0 0 2.5 16h11a2.5 2.5 0 0 0 2.5-2.5v-11A2.5 2.5 0 0 0 13.5 0h-11ZM1 2.5A1.5 1.5 0 0 1 2.5 1h11A1.5 1.5 0 0 1 15 2.5v11a1.5 1.5 0 0 1-1.5 1.5h-11A1.5 1.5 0 0 1 1 13.5v-11Z"/>
-</svg></a>
+          ><svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            fill="currentColor"
+            class="bi bi-explicit"
+            viewBox="0 0 16 16"
+          >
+            <path d="M6.826 10.88H10.5V12h-5V4.002h5v1.12H6.826V7.4h3.457v1.073H6.826v2.408Z" />
+            <path
+              d="M2.5 0A2.5 2.5 0 0 0 0 2.5v11A2.5 2.5 0 0 0 2.5 16h11a2.5 2.5 0 0 0 2.5-2.5v-11A2.5 2.5 0 0 0 13.5 0h-11ZM1 2.5A1.5 1.5 0 0 1 2.5 1h11A1.5 1.5 0 0 1 15 2.5v11a1.5 1.5 0 0 1-1.5 1.5h-11A1.5 1.5 0 0 1 1 13.5v-11Z"
+            /></svg
+        ></a>
 
         <!-- Email -->
-        <a :href="'mailto:' + artist.email">  <b-icon-envelope style="font-size: 30px; color: black;"></b-icon-envelope></a>
+        <a :href="'mailto:' + artist.email"> <b-icon-envelope style="font-size: 30px; color: black"></b-icon-envelope></a>
       </div>
     </div>
 
