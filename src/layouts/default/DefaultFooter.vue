80151937936da5e34bb98323de87a4d5214c1a17
tedet <tedteah@gmail.com>
2023-05-06 07:19:09 -0400

    message

--- src/layouts/default/DefaultFooter.vue
+++ src/layouts/default/DefaultFooter.vue
@@ -1,7 +1,35 @@
 <template>
   <footer class="footer">
     <div class="container">
-      <span>Developed by</span> <a href="https://github.com/kouts" target="_blank" rel="noopener">@kouts</a>
+      <div class="row">
+        <div class="col" style="font-size: 16px; margin-top: 15px">
+          <a
+            class="link-light"
+            href="https://www.facebook.com/Island-Craft-Shop-189477107771345/"
+            alt="FB"
+            style="color: white; padding-right: 5px"
+            ><b-icon-facebook></b-icon-facebook
+          ></a>
+          <a href="https://www.instagram.com/vtislandcraftshop" alt="insta" style="color: white; padding-right: 5px"
+            ><b-icon-instagram></b-icon-instagram
+          ></a>
+          <a href="https://www.instagram.com/vtislandcraftshop" alt="twitter" style="color: white; padding-right: 5px"
+            ><b-icon-twitter></b-icon-twitter
+          ></a>
+
+          <b-icon-envelope style="padding-right: 5px; color: white"></b-icon-envelope>
+        </div>
+        <div class="col" style="font-size: 10px; color: white">
+          <br />
+          <b-icon-telephone-inbound-fill style="color: white"></b-icon-telephone-inbound-fill>
+          <br />
+          <br />802.922.0202
+        </div>
+        <div style="font-size: 10px; margin-top: 10px; color: white" class="col">
+          329 U.S. Rte 2<br />
+          South Hero, VT 05486
+        </div>
+      </div>
     </div>
   </footer>
 </template>
